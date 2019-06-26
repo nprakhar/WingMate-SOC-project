182e7e43d7cbf772c98e95dca3e446c970fa8524
nprakhar <nprakhar@iitk.ac.in>
2019-06-26 18:16:49 +0000

    pages added

--- wing_mate/lib/ui/home.dart
+++ wing_mate/lib/ui/home.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
+import 'chats.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+
     return new Scaffold(
       appBar: new AppBar(
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.lightBlue,
         title: new Text("WingMate"),
+        elevation: 10.0,
         actions: <Widget>[
           new IconButton(icon: new Icon(Icons.search), onPressed: () => debugPrint("Search")),
           new IconButton(icon: new Icon(Icons.dehaze), onPressed: () => debugPrint("About")),
         ],
       ),
+      body: new ChatScreen(),
       floatingActionButton: FloatingActionButton(
         onPressed: () => debugPrint("All Contacts"),
         tooltip: 'All Contacts',
         child: Icon(Icons.message),
+        backgroundColor: Colors.blue,
       ),
-
+      bottomNavigationBar: new BottomNavigationBar(
+          backgroundColor: Colors.lightBlueAccent,
+          elevation: 10.0,
+          items: [
+            new BottomNavigationBarItem(icon: new Icon(Icons.chat_bubble), title: new Text("Chats")),
+            new BottomNavigationBarItem(icon: new Icon(Icons.event), title: new Text("Events")),
+            new BottomNavigationBarItem(icon: new Icon(Icons.music_note), title: new Text("Group Play")),
+          ], onTap: (int i){
+            if(i==1) Navigator.pushNamed(context, '/events_sl');
+            else if(i==2) Navigator.pushNamed(context, '/group_play_sl');
+          },)
     );
   }
-
 }
